7853e5a937efc9f7afa07da1442f441220f109a4
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-16 14:49:23 -0400

    Add Card wrapper component and CSS module for styling

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,16 +1,20 @@
+import Card from '../UI/Card';
+
 const AddUser = props => {
   const submitHandler = event => {
     event.preventDefault();
   };
 
   return (
-    <form onSubmit={submitHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age (Years)</label>
-      <input id="age" type="number" />
-      <button type="submit">Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={submitHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" />
+        <label htmlFor="age">Age (Years)</label>
+        <input id="age" type="number" />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
   );
 };
 

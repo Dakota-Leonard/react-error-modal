095ffe0e04ef813b0e6703c31dc3bb8c24ac7e6a
Dakota Leonard <dakotaleonard.dev@gmail.com>
2021-10-16 14:38:10 -0400

    Create AddUser component and HTML for it

---
+++ src/Components/Users/AddUser.js
@@ -0,0 +1,13 @@
+const AddUser = props => {
+  return (
+    <form>
+      <label htmlFor="username">Username</label>
+      <input id="username" type="text" />
+      <label htmlFor="age">Age (Years)</label>
+      <input id="age" type="number" />
+      <button type="submit">Add User</button>
+    </form>
+  );
+};
+
+export default AddUser;
